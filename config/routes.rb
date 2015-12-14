fec1633818fdc61953fa67e2012e80487b7e1c09
Marlo Major <MarloMajor@yahoo.com>
2015-12-14 15:03:53 -0700

    user login functionality
    - User can login with Twitter
    - User can logout with Twitter

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root "welcome#index"
 
   get "/auth/twitter", as: :login
+  get "/auth/twitter/callback", to: "sessions#create"
+  get "/logout", as: :logout, to: "sessions#destroy"
 end
