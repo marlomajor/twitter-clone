40c0625081bcfa7206e2a97da21f6f2b04abbd55
Marlo Major <MarloMajor@yahoo.com>
2015-12-15 17:31:43 -0700

    Added more than heroku...
    - Added tweets controller and model
    - tweet service not rendering yet

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
+require
+
 class WelcomeController < ApplicationController
   def index
-    
+    if current_user.present?
+      binding.pry
+      @service = TwitterService.new(current_user)
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root "welcome#index"
 
+  resources :tweets
+
+  get "/complete_info/:id", to: "tweets#complete_info"
+
   get "/auth/twitter", as: :login
   get "/auth/twitter/callback", to: "sessions#create"
   get "/logout", as: :logout, to: "sessions#destroy"
