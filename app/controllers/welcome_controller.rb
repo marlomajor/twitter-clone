633a2d19686cf6cbcd4a43f4049df8f0c052e3cf
Marlo Major <MarloMajor@yahoo.com>
2015-12-15 17:33:06 -0700

    remove pry

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,6 @@
-require
-
 class WelcomeController < ApplicationController
   def index
     if current_user.present?
-      binding.pry
       @service = TwitterService.new(current_user)
     end
   end
