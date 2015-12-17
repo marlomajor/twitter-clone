f110b7724758842deb5c8287f9fc2d2d83ac28f6
Marlo Major <MarloMajor@yahoo.com>
2015-12-17 13:15:23 -0700

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,20 @@
 class User < ActiveRecord::Base
-  include ::Client
 
   def self.from_omniauth(auth_info)
     where(uid: auth_info[:uid]).first_or_create do |new_user|
       new_user.uid                = auth_info.uid
       new_user.name               = auth_info.extra.raw_info.name
-      new_user.screen_name        = auth_info.extra.raw_info.screen_name
       new_user.oauth_token        = auth_info.credentials.token
       new_user.oauth_token_secret = auth_info.credentials.secret
-      new_user.image              = auth_info.info.image
-      new_user.friends_count      = auth_info.extra.raw_info.friend_count
-      new_user.follower_count     = auth_info.extra.raw_info.followers_count
     end
+    # user = User.find_by(uid: auth_info.uid)
+    # user.screen_name              = auth_info.extra.raw_info.screen_name
+    # user.image                    = auth_info.info.image
+    # user.description              = auth_info.info.description
+    # user.followers_count          = auth_info.extra.raw_info.followers_count
+    # user.friends_count            = auth_info.extra.raw_info.friends_count
+    # user.save
+    # user
   end
 
-  def tweets
-    twitter_client(self).user_timeline(screen_name)
-  end
 end
